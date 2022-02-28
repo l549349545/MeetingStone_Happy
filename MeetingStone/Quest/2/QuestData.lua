522e96064b67fbfb0cf0ba45606c5c750ce3e1d8
白描 <306941068@qq.com>
2022-02-28 22:09:25 +0800

    Init Signed-off-by: 白描

---
+++ MeetingStone/Quest/2/QuestData.lua
@@ -0,0 +1,21 @@
+-- QuestData.lua
+-- @Author : Dencer (tdaddon@163.com)
+-- @Link   : https://dengsir.github.io
+-- @Date   : 4/21/2021, 10:56:19 AM
+--
+BuildEnv(...)
+
+if not ADDON_REGIONSUPPORT then
+    return
+end
+
+QUEST_CFG_DATA = { --
+    [1] = {
+        title = [[{{GetTimeLimitText}}通关{{progressMaxValue}}次{{challengeLevel}}层及以上史诗钥石地下城]],
+        proto = {'challengeLevel', 'timeLimit'},
+    },
+    [2] = {
+        title = [[]],
+        proto = {},
+    },
+}
